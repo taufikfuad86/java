4b0148e3b6cd639505d77a5e4a09062f07d1f936
Roberto <taufiqfuad@gmail.com>
2018-04-06 15:29:40 +0800

    Membuat Class

---
+++ src/java01/Pemimpin.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pemimpin;
+
+/**
+ *
+ * @author WAHYU'S PC
+ */
+public class Pemimpin {
+    String Presiden;
+    String Gubernur;
+    String Walikota;
+    String Camat;
+    String Lurah;
+    String Kades;
+    String RW;
+    String RT;
+    String KK;
+    
+    void cetakpemimpin(){
+    
+    }
+    
+}
